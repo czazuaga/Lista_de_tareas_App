2a1e8c28c40e5c9968044740c2602d3e0f3e7df1
czazu <czazuaga@gmail.com>
2016-05-08 16:03:10 +0200

    Finishing the DataBaseHelper class

---
+++ app/src/main/java/com/example/czazu/dblibro/ItemActivity.java
@@ -0,0 +1,12 @@
+package com.example.czazu.dblibro;
+
+import android.app.Activity;
+
+/**
+ * Created by czazu on 08/05/2016.
+ */
+public class ItemActivity extends Activity {
+
+
+
+}
--- app/src/main/java/com/example/czazu/dblibro/MainActivity.java
+++ app/src/main/java/com/example/czazu/dblibro/MainActivity.java
@@ -2,9 +2,39 @@ package com.example.czazu.dblibro;
 
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends ListActivity {
+
+    public static final int NEW_ITEM = 1;
+    public static final int EDIT_ITEM = 2;
+    public static final int SHOW_ITEM = 3;
+
+    private int mLastrowSelected = 0;
+    public static  DataBaseHelper dataBaseHelper=null;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_item:
+                Intent intent = new Intent(this,ItemActivity.class);
+                startActivityForResult(intent,NEW_ITEM);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
